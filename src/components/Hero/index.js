4e38de143107a24cd0cf344928b134cfee1fea3b
Leslie Lopez <leslie2515lopez@gmail.com>
2021-07-03 21:53:59 -0500

    added sidebar and product section and components

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navbar";
 import {
   HeroContainer,
@@ -8,11 +8,19 @@ import {
   HeroP,
   HeroBtn
 } from "./HeroElements";
+import Sidebar from "../Sidebar";
 
 const Hero = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <HeroContainer>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
       <HeroContent>
         <HeroItems>
           <HeroH1>Greatest Pizza Ever</HeroH1>
