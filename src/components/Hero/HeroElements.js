bd168b3aa62b1c406fa3a5a97ae0aff300bae6f8
Leslie Lopez <leslie2515lopez@gmail.com>
2021-07-03 21:56:00 -0500

    fixed minor issue in HeroElements file

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -4,9 +4,9 @@ import ImgBg from "../../images/pizza-3.jpg";
 export const HeroContainer = styled.div`
   background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
     url(${ImgBg});
-    height: 100vh;
-    background-position: center;
-    background-size: cover:
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
 `;
 
 export const HeroContent = styled.div`
